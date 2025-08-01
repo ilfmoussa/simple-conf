23b79afff9290a227b79bd3628194757fc6415c4
Ilf Moussa <ilf.moussa@gmail.com>
2025-08-01 21:46:44 +0200

    update script

--- fast-config.sh
+++ fast-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt install -y terminator keepass2
+sudo apt install -y terminator keepass2 htop
+sudo sed -i 's|#WaylandEnable|WaylandEnable|g' /etc/gdm3/custom.conf
 mkdir ~/.config/terminator
 perl gnome-shortcuts/keybindings.pl --import gnome-shortcuts/current.key
 cp terminator/config ~/.config/terminator/
